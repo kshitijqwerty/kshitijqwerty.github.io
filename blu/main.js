893729ac4e2f2653bf6a2cb0d07192f5c9a54546
Kshitij Gupta <kshitij15048@iiitd.ac.in>
2023-02-08 07:46:34 +0530

    Add files via upload

---
+++ blu/main.js
@@ -0,0 +1,32 @@
+var bleno = require('bleno');
+
+var BlenoPrimaryService = bleno.PrimaryService;
+
+var EchoCharacteristic = require('./characteristic');
+
+console.log('bleno - echo');
+
+bleno.on('stateChange', function(state) {
+  console.log('on -> stateChange: ' + state);
+
+  if (state === 'poweredOn') {
+    bleno.startAdvertising('kshitij', ['c2a913ee-a6dd-11ed-afa1-0242ac120002']);
+  } else {
+    bleno.stopAdvertising();
+  }
+});
+
+bleno.on('advertisingStart', function(error) {
+  console.log('on -> advertisingStart: ' + (error ? 'error ' + error : 'success'));
+
+  if (!error) {
+    bleno.setServices([
+      new BlenoPrimaryService({
+        uuid: 'ec00',
+        characteristics: [
+          new EchoCharacteristic()
+        ]
+      })
+    ]);
+  }
+});
